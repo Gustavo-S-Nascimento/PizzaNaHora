9848ce4e38206adda0aeb55abd4a3c7c48057e2b
Gustavo dos Santos Nascimento <74427958+GustavoSantoz@users.noreply.github.com>
2024-05-28 04:08:04 +0000

    fix: github pages error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import path from "path"
+import { resolve } from 'path';
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
+  base: '/PizzaNaHora/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
