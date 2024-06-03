d40f6e6f29923fd5e546e023d337969ca05e0c1c
Gustavo dos Santos Nascimento <74427958+GustavoSantoz@users.noreply.github.com>
2024-06-03 03:57:32 +0000

    create: login and register

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,14 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 
-import { CardContent, Card } from "@/components/ui/card"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 import { SVGProps } from "react"
 import { JSX } from "react/jsx-runtime"
 import {
@@ -17,6 +24,22 @@ import {
 } from "@/components/ui/sheet"
 import { Separator } from "@/components/ui/separator"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+
 
 
 
@@ -39,10 +62,73 @@ export default function Home() {
                         <a className="flex justify-center items-center h-8 text-gray-600 border p-[1px] border-transparent hover:text-white hover:border hover:p-[1px] hover:rounded hover:border-red-500 hover:bg-red-500 transition-all" href="#Menu">
                             Destaques
                         </a>
-                        <Avatar>
-                            <AvatarImage src="/CatAvatar.png" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
+                        <Dialog>
+                            <DialogTrigger><Avatar>
+                                <AvatarImage src="/CatAvatar.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar></DialogTrigger>
+                            <DialogContent className="flex justify-center">
+                                <Tabs defaultValue="account" className="w-[400px]">
+                                    <TabsList className="grid w-full grid-cols-2">
+                                        <TabsTrigger value="login">Login</TabsTrigger>
+                                        <TabsTrigger value="register">Register</TabsTrigger>
+                                    </TabsList>
+                                    <TabsContent value="login">
+                                        <Card>
+                                            <CardHeader>
+                                                <CardTitle>Sign in</CardTitle>
+                                                <CardDescription>
+                                                    Log in with your account.
+                                                </CardDescription>
+                                            </CardHeader>
+                                            <CardContent className="space-y-2">
+                                                <div className="space-y-1">
+                                                    <Label htmlFor="username">Username</Label>
+                                                    <Input id="username" placeholder="@JohnDoe" />
+                                                </div>
+                                                <div className="space-y-1">
+                                                    <Label htmlFor="password-login">Password</Label>
+                                                    <Input id="password-login" type="password" />
+                                                </div>
+
+                                            </CardContent>
+                                            <CardFooter>
+                                                <Button>Enter</Button>
+                                            </CardFooter>
+                                        </Card>
+                                    </TabsContent>
+                                    <TabsContent value="register">
+                                        <Card>
+                                            <CardHeader>
+                                                <CardTitle>Sign up</CardTitle>
+                                                <CardDescription>
+                                                    Create your account.
+                                                </CardDescription>
+                                            </CardHeader>
+                                            <CardContent className="space-y-2">
+                                                <div className="space-y-1">
+                                                    <Label htmlFor="name">Name</Label>
+                                                    <Input id="name" placeholder="John Doe" />
+                                                </div>
+                                                <div className="space-y-1">
+                                                    <Label htmlFor="username">Username</Label>
+                                                    <Input id="username" placeholder="@JohnDoe" />
+                                                </div>
+                                                <div className="space-y-1">
+                                                    <Label htmlFor="password-register">Password</Label>
+                                                    <Input id="password-register" type="password" />
+                                                </div>
+                                            </CardContent>
+                                            <CardFooter>
+                                                <Button>Create account</Button>
+                                            </CardFooter>
+                                        </Card>
+                                    </TabsContent>
+                                </Tabs>
+                            </DialogContent>
+                        </Dialog>
+
+
 
                     </nav>
                     <div className="md:hidden">
